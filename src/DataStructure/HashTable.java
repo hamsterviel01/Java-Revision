657bebe62459235d0b2d87103278963477878826
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2015-01-21 17:22:08 +0800

    Add hash table

---
+++ src/DataStructure/HashTable.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package DataStructure;
+
+/**
+ * @author tuananhn
+ *
+ */
+public class HashTable {
+	protected void chainingCollisionManage(){
+		
+	}
+	
+	protected void openAdressingCollisionManage(){
+		
+	}
+	
+	protected void divisionHashFunction(){
+		
+	}
+	
+	protected void multiplicationHashFunction(){
+		
+	}
+	
+	protected void universalHashingHashFunction(){
+		
+	}
+}
