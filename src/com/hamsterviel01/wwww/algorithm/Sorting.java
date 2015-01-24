a10c473c4d65dbe4a31ccd0cf4b1e0db6d7f9e6e
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2015-01-24 10:34:40 +0800

    Add sorting algorithm

--- src/com/hamsterviel01/wwww/algorithm/Sorting.java
+++ src/com/hamsterviel01/wwww/algorithm/Sorting.java
@@ -1,5 +1,17 @@
 package com.hamsterviel01.wwww.algorithm;
 
 public class Sorting {
-
+	private int[] array;
+	
+	public Sorting(int[] array){
+		this.array = array;
+	}
+	
+	protected void insertionSort(){
+		
+	}
+	
+	protected void mergeSort(){
+		
+	}
 }
