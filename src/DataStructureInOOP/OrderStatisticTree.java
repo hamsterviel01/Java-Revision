a94a9e0e48465d9dc7b57d6dfbf1a0692e7d7e13
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2015-02-04 15:25:18 +0800

    No comment

--- src/DataStructureInOOP/OrderStatisticTree.java
+++ src/DataStructureInOOP/OrderStatisticTree.java
@@ -7,7 +7,7 @@ package DataStructureInOOP;
  * @author tuananhn
  *
  */
-public class OrderStatisticTree {
+public class OrderStatisticTree extends RedBlackTree{
 	public OrderStatisticTree(int rootIndex, int treeMaxNumberOfNode, int[] keyArray, int[] parentArray, int[] leftChildArray, int[] rightChildArray, int[] satelliteData, String[] color){
 		super(rootIndex, treeMaxNumberOfNode, keyArray, parentArray, leftChildArray, rightChildArray, satelliteData, color);
 	}
