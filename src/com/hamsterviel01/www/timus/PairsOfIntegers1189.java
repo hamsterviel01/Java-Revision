18f1fd112eb804bd4f43a9ef1b43a497c860d020
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2015-03-16 18:02:50 +0800

    Fix few bug, but still have some in 1189

--- src/com/hamsterviel01/www/timus/PairsOfIntegers1189.java
+++ src/com/hamsterviel01/www/timus/PairsOfIntegers1189.java
@@ -1,50 +1,56 @@
 package com.hamsterviel01.www.timus;
 
 import java.math.*;
+import java.util.*;
 
 public class PairsOfIntegers1189 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner in = new Scanner(System.in);
+		int count = calculatePairsOfInteger(in.nextInt());
+		System.out.println("Number of pairs: " + count);
 	}
 	
-	public int calculatePairsOfInteger(int n){
+	public static int calculatePairsOfInteger(int n){
 		int count = 0;
 		boolean even = (n%2 == 0);
+		int[] digits = new int[String.valueOf(n).length() - 1];
 		
 		//i must >= 1 because n has at least 2 digits
-		for (int i = 0; i<=String.valueOf(n).length(); i++){
-			for (int m = 0; m <= 9; m++){
-				//give a1 value, calculate a2
-				//n = 11*a1*10^i + 2*a2 + m*10^i
-				int tenPowerI = tenToPow(i);
-				
-				for (int a1 = 1; 11*a1*tenPowerI <= n; a1++){
-					//check if a1 and n with odd and even
-					if (i!=0){
-						if (even) {
-							int a2 = (n - m*tenPowerI - 11*a1*tenPowerI)/2;
-							count++;
-							constructIntegerPair(a1, a2, m, i);
-						}
-					} else {
-						
+		for (int i = 0; i<=String.valueOf(n).length() - 1; i++){
+			//give a1 value, calculate a2
+			//n = 11*a1*10^i + 2*a2 + m*10^i
+			int tenPowerI = tenToPow(i);
+			int m = n/tenPowerI;
+			
+			for (int a1 = 0; 11*a1*tenPowerI + m*tenPowerI < n; a1++){
+				//check if a1 and n with odd and even
+				if (i!=0){
+					if (even) {
+						int a2 = (n - m*tenPowerI - 11*a1*tenPowerI)/2;
+						count++;
+						System.out.println(Arrays.toString(constructIntegerPair(a1, a2, m, i)));
+					}
+				} else {
+					if ((a1 + n + m)%2 == 0){
+						int a2 = (n - m*tenPowerI - 11*a1*tenPowerI)/2;
+						count++;
+						System.out.println(Arrays.toString(constructIntegerPair(a1, a2, m, i)));
 					}
 				}
-				
 			}
 		}
 		
 		return count;
 	}
 	
-	public int[] constructIntegerPair(int a1, int a2, int m, int i){
+	static public int[] constructIntegerPair(int a1, int a2, int m, int i){
 		int tenPowerI = tenToPow(i);
-		return new int[]{a1*tenPowerI*10 + m*tenPowerI + a2, a1*tenPowerI + a2};
+		return new int[]{a1, a2, m, i, a1*tenPowerI*10 + m*tenPowerI + a2, a1*tenPowerI + a2};
 	}
 	
-	public int tenToPow(int i){
+	static public int tenToPow(int i){
 		return (int)Math.pow(10.0, (double)(i));
 	}
 
--- src/com/hamsterviel01/www/timus/ProductOfDigits1014.java
+++ src/com/hamsterviel01/www/timus/ProductOfDigits1014.java
@@ -56,3 +56,81 @@ public class ProductOfDigits1014 {
 		}
 	}
 }
+
+/**
+ * Solution from xukien - 0.078s
+ * 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+
+public class No1014 {
+
+    public static     Stack<Integer> sk= new Stack<Integer>();
+    public static boolean isprime(int x)
+    {
+        if (x<2) return false;
+        if (x==2) return true;
+        if(x%2==0) return false;
+        for (int i=3;i*i<=x;i+=2)
+            if (x%i==0) return false;
+        return true;
+    }
+
+    public static void run(int x)
+    {
+
+        if (x/10==0)
+        {
+            if (x==0) sk.push(10);
+            else    sk.push(x);
+            return;
+        }
+        if (isprime(x))
+        {
+            sk.push(-1);
+            return;
+        }
+
+        while (x!=1)
+        {
+            int i;
+            for(i=9; i>1; i--)
+                if( x%i==0)
+                {
+                    sk.push(i);
+                    x=x/i;
+                    if(isprime(x)&&(x>10))
+                    {
+                        sk.clear();
+                        sk.push(-1);
+                        return;
+                    }
+                    break;
+                }
+            if (i==1)
+            {
+                sk.clear();
+                sk.push(-1);
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader rd =new BufferedReader( new InputStreamReader(System.in));
+        int n=Integer.parseInt(rd.readLine());
+        run(n);
+        String temp="";
+        while(!sk.isEmpty())
+        {
+            temp+=sk.pop().toString();
+        }
+            System.out.print(temp);
+        System.exit(0);
+    }
+
+}
+ * */
